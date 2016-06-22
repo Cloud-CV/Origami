ca728dcfeb3ccaa16cc058ac9198af90d313721c
Ashish Chaudhary <me@ashishchaudhary.in>
2016-06-22 16:12:04 +0530

    removed and changed local application credentials from  config.sample.js

--- outCalls/config.sample.js
+++ outCalls/config.sample.js
@@ -1,8 +1,8 @@
 const appConfig = {
   CLIENT_IP: "0.0.0.0",
-  CLIENT_ID: "ca52c7ffdae5d3454f5b",
-  CLIENT_SECRET: "df3e74ee7b14b35dd0bad092bcc489b4c6e9b6b9",
-  APP_SECRET: "tocttou rokz"
+  CLIENT_ID: "",
+  CLIENT_SECRET: "",
+  APP_SECRET: ""
 };
 
 module.exports = appConfig;
