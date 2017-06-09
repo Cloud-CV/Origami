123884171ff00b1a21c955b44edaa1fc97e5bb2c
Avais Pagarkar <avaispagarkar123@gmail.com>
2017-06-10 01:19:11 +0530

    Removed old docs (#44)

--- src/routes.js
+++ src/routes.js
@@ -7,24 +7,6 @@ import HomePageComponent from "./components/home/HomePage";
 import URLShortenerComponent from "./components/urlShortener";
 import ShareProfile from "./components/user/userShareProfile";
 
-import LibConfigDocsPageComponent
-  from "./components/docs/libdocs/Configuration";
-import LibRegistrationDocsPageComponent
-  from "./components/docs/libdocs/Registration";
-import LibInputDocsPageComponent from "./components/docs/libdocs/Input";
-import LibOutputDocsPageComponent from "./components/docs/libdocs/Output";
-import LibTerminalDocsPageComponent from "./components/docs/libdocs/Terminal";
-
-import GettingStartedConfigurationPageComponent
-  from "./components/docs/gettingstarted/Configuration";
-import GettingStartedCreatePageComponent
-  from "./components/docs/gettingstarted/Create";
-import GettingStartedIOPageComponent from "./components/docs/gettingstarted/IO";
-import GettingStartedPublishPageComponent
-  from "./components/docs/gettingstarted/Publish";
-import GettingStartedModifyPageComponent
-  from "./components/docs/gettingstarted/Modify";
-
 import NonGHUserProfileComponent from "./components/user/nonghUserProfile";
 
 import LoginComponent from "./components/stateless/login";
@@ -42,27 +24,6 @@ import NGHDemoPageComponent
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePageComponent} />
-
-    <Redirect from="libdocs" to="/libdocs/configuration" />
-    <Route path="libdocs">
-      <Route path="configuration" component={LibConfigDocsPageComponent} />
-      <Route path="registration" component={LibRegistrationDocsPageComponent} />
-      <Route path="input" component={LibInputDocsPageComponent} />
-      <Route path="output" component={LibOutputDocsPageComponent} />
-      <Route path="terminal" component={LibTerminalDocsPageComponent} />
-    </Route>
-
-    <Redirect from="gettingstarted" to="/gettingstarted/configuration" />
-    <Route path="gettingstarted">
-      <Route
-        path="configuration"
-        component={GettingStartedConfigurationPageComponent}
-      />
-      <Route path="create" component={GettingStartedCreatePageComponent} />
-      <Route path="io" component={GettingStartedIOPageComponent} />
-      <Route path="publish" component={GettingStartedPublishPageComponent} />
-      <Route path="modify" component={GettingStartedModifyPageComponent} />
-    </Route>
 
     <Route path="ngh/user" component={NonGHUserProfileComponent} />
     <Route path="ngh/user/register" component={RegisterNonGHPageComponent} />
