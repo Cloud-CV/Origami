b6636ec76dcb61afb67b7b7362f86673505b307f
Avais Pagarkar <avaispagarkar123@gmail.com>
2017-07-05 02:17:04 +0530

    Import only required classes

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import *
+from api.models import Demo, InputComponent, OutputComponent, Permalink, RootSettings
 
 
 class DemoSerializer(serializers.ModelSerializer):
