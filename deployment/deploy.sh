283fce4156f9e1871bfef2c0cf297db854cb9791
Avais Pagarkar <avaispagarkar123@gmail.com>
2017-07-14 23:56:11 +0530

    Run multiple uwsgi processes

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -16,4 +16,4 @@ mkdir media/
 python manage.py collectstatic --noinput
 sudo /etc/init.d/nginx restart
 env NODE_ENV="production" node_modules/.bin/webpack --config webpack.prod.config.js
-daphne -b 0.0.0.0 -p 8001 django_server.asgi:channel_layer & python manage.py runworker & python manage.py runworker & uwsgi --socket :8000 --module django_server.wsgi 
+daphne -b 0.0.0.0 -p 8001 django_server.asgi:channel_layer & python manage.py runworker & python manage.py runworker & uwsgi --socket :8000 --module django_server.wsgi --processes 4 --threads 2
