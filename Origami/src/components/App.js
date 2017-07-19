4539112e53e82b794838295075c87541c1478dc0
Avais Pagarkar <avaispagarkar123@gmail.com>
2017-07-19 22:44:13 +0530

    Pass through prettier

--- Origami/src/components/App.js
+++ Origami/src/components/App.js
@@ -112,36 +112,29 @@ class App extends React.Component {
   handleClick(e) {
     if (!this.state.login && e.key === "2") {
       this.initiateLogin();
-    }
-    else if(e.key == "3") {
+    } else if (e.key == "3") {
       this.getDocs();
     }
   }
 
   handleClickAfterLogin(e) {
-    if (e.key === "1"){
+    if (e.key === "1") {
       userApi
         .userProfileFromName(localStorage.getItem("username"))
         .then(user => {
-          user = JSON.parse(user)
-          browserHistory.push(
-              `/u/${user.login}/${user.id}`
-          )
-        })
+          user = JSON.parse(user);
+          browserHistory.push(`/u/${user.login}/${user.id}`);
+        });
     }
-    if(e.key === "2") {
+    if (e.key === "2") {
       browserHistory.push("/");
-    }
-    else if(e.key === "3") {
+    } else if (e.key === "3") {
       browserHistory.push("/ngh/user");
-    }
-    else if(e.key === "4"){
+    } else if (e.key === "4") {
       browserHistory.push("/ngh/user/register");
-    }
-    else if(e.key === "5"){
+    } else if (e.key === "5") {
       this.getDocs();
-    }
-    else if(e.key === "6"){
+    } else if (e.key === "6") {
       this.logout();
     }
   }
@@ -157,31 +150,53 @@ class App extends React.Component {
     }
     if (this.state.login) {
       return (
-        <Layout style={{ height: '100vh', background: '#FEFEFE' }}>
-          <Sider style={{ overflow: 'auto', background: '#FEFEFE', 'box-shadow': '10px 0px 20px #E0E0E0' }} width='250'>
-            <div className="logo" style = {{height: '31px', 'border-radius': '6px', margin: '16px', color: 'black' }}> Origami logo </div>
-            <Menu style={{background: '#FEFEFE'}} mode="inline" defaultSelectedKeys={['3']} onClick={this.handleClickAfterLogin}>
-              <Menu.Item key="1" style={{'font-size': '16px'}}>
+        <Layout style={{ height: "100vh", background: "#FEFEFE" }}>
+          <Sider
+            style={{
+              overflow: "auto",
+              background: "#FEFEFE",
+              "box-shadow": "10px 0px 20px #E0E0E0"
+            }}
+            width="250"
+          >
+            <div
+              className="logo"
+              style={{
+                height: "31px",
+                "border-radius": "6px",
+                margin: "16px",
+                color: "black"
+              }}
+            >
+              {" "}Origami logo{" "}
+            </div>
+            <Menu
+              style={{ background: "#FEFEFE" }}
+              mode="inline"
+              defaultSelectedKeys={["3"]}
+              onClick={this.handleClickAfterLogin}
+            >
+              <Menu.Item key="1" style={{ "font-size": "16px" }}>
                 <Icon type="user" />
                 <span className="nav-text">Profile</span>
               </Menu.Item>
-              <Menu.Item key="2" style={{'font-size': '16px'}}>
+              <Menu.Item key="2" style={{ "font-size": "16px" }}>
                 <Icon type="video-camera" />
                 <span className="nav-text">Discover</span>
               </Menu.Item>
-              <Menu.Item key="3" style={{'font-size': '16px'}}>
+              <Menu.Item key="3" style={{ "font-size": "16px" }}>
                 <Icon type="cloud-o" />
                 <span className="nav-text">My demos</span>
               </Menu.Item>
-              <Menu.Item key="4" style={{'font-size': '16px'}}>
+              <Menu.Item key="4" style={{ "font-size": "16px" }}>
                 <Icon type="plus-circle-o" />
                 <span className="nav-text">Create Demo</span>
               </Menu.Item>
-              <Menu.Item key="5" style={{'font-size': '16px'}}>
+              <Menu.Item key="5" style={{ "font-size": "16px" }}>
                 <Icon type="question-circle-o" />
                 <span className="nav-text">Help / WebApp Docs</span>
               </Menu.Item>
-              <Menu.Item key="6" style={{'font-size': '16px'}}>
+              <Menu.Item key="6" style={{ "font-size": "16px" }}>
                 <Icon type="logout" />
                 <span className="nav-text">Logout</span>
               </Menu.Item>
--- Origami/src/components/home/HomePage.js
+++ Origami/src/components/home/HomePage.js
@@ -72,9 +72,12 @@ class HomePageComponent extends React.Component {
         if (rootData.is_cloudcv) {
           this.setState({ is_cloudcv: true });
           getAllPermalink().then(links => {
-            getAllDemosByCloudCV(rootData.root_user_github_login_id).then(demos => {
+            getAllDemosByCloudCV(
+              rootData.root_user_github_login_id
+            ).then(demos => {
               const relevantLink = JSON.parse(links).filter(
-                x => parseInt(x.user_id, 10) === rootData.root_user_github_login_id
+                x =>
+                  parseInt(x.user_id, 10) === rootData.root_user_github_login_id
               );
               const allDemos = [];
               JSON.parse(demos).map((demo, index) => {
@@ -132,8 +135,8 @@ class HomePageComponent extends React.Component {
     const styles = this.getStyles();
     return (
       <div>
-      {this.props.login && 
-        <Header id="layout-header">
+        {this.props.login &&
+          <Header id="layout-header">
             <Row>
               <Col span={3} offset={1}>
                 <h2 id="logo-title">
@@ -152,8 +155,7 @@ class HomePageComponent extends React.Component {
                 </Button>
               </Col>
             </Row>
-          </Header>
-      }
+          </Header>}
         <Content id="content" style={styles.content}>
           <div id="content-div" style={styles.contentDiv}>
             <Row>
