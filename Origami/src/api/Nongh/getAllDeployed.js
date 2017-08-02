0de9ba41740bd346f1d8bf6079d779a8f7d8a6cd
Avais Pagarkar <avaispagarkar123@gmail.com>
2017-08-02 20:20:35 +0530

    Add getAllDemo and search demo (#58)

---
+++ Origami/src/api/Nongh/getAllDeployed.js
@@ -0,0 +1,16 @@
+import request from "superagent";
+const appConfig = require("../../../outCalls/config");
+import { baseURL } from "../CommonLocal/baseURL";
+
+export function getAllDeployed() {
+  let URL = `${baseURL}/api/demo/`;
+  return new Promise((resolve, reject) => {
+    request.get(URL).set("Accept", "application/json").end((err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.text);
+      }
+    });
+  });
+}
