86f675bbb7bcb5279086b9d21ac5dc1b66412b68
VIPIN SINGH <mailme.vipin475@gmail.com>
2018-01-03 14:26:26 +0530

    Update PieChartOutputShowcaseCard.js

--- Origami/src/components/outputcomponents/PieChartOutput/PieChartOutputShowcaseCard.js
+++ Origami/src/components/outputcomponents/PieChartOutput/PieChartOutputShowcaseCard.js
@@ -31,15 +31,12 @@ class PieChartOutputShowcaseCard extends OutputShowcaseCard {
     } else {
       let propsToStore = [];
       this.state.headers.map(header => {
-           if(typeof header=='object')
-        {
-          propsToStore.push({id:"",label: header["label"]});
-
-        }
-        else
-        {
-          propsToStore.push({id:"",label: header});
-        }
+      if(typeof header=='object'){
+       propsToStore.push({id:"",label: header["label"]});
+      }
+      else{
+       propsToStore.push({id:"",label: header});
+       }
       });
       this.outputComponentDemoModelActions
         .updateOutputComponentModel({
