ae86c5d2dbbdaa9d0dbc614a5106debed9143431
Pavan <pavan.a.123@outlook.com>
2018-01-04 10:47:12 -0600

    should be working now

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 sudo docker run -d -p 6379:6379 --name origami-redis redis:alpine
 sudo docker start origami-redis
 tmux new -s server -d
-tmux send-keys -t server 'source ~/venv/bin/activate && source origami.env && python manage.py runserver --noworker' C-m &
-mkfifo /tmp/tmuxpipeworker
+VENV="~/venv/bin/activate"
+tmux send-keys -t server 'source '"$VENV"' && source origami.env && python manage.py runserver --noworker' C-m &
+mkfifo /tmp/tmuxpipeserver
 tmux pipe-pane -o -t server 'cat >> /tmp/tmuxpipeserver'
 cat /tmp/tmuxpipeserver & 
 tmux new -s worker -d
-tmux send-keys -t worker 'source ~/venv/bin/activate && source origami.env && python manage.py runworker &' C-m &
+tmux send-keys -t worker 'source '"$VENV"' && source origami.env && python manage.py runworker &' C-m &
 mkfifo /tmp/tmuxpipeworker
 tmux pipe-pane -o -t worker 'cat >> /tmp/tmuxpipeworker'
 cat /tmp/tmuxpipeworker & 
