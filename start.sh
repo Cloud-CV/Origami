7d7b8c1085ebc9b7691a597b0209158b5a4855b0
Pavan <pavan.a.123@outlook.com>
2018-01-04 09:24:13 -0600

    pipes and tmux kill!

--- start.sh
+++ start.sh
@@ -2,7 +2,24 @@ sudo docker run -d -p 6379:6379 --name origami-redis redis:alpine
 sudo docker start origami-redis
 tmux new -s server -d
 tmux send-keys -t server 'source ~/venv/bin/activate && source origami.env && python manage.py runserver --noworker' C-m &
+mkfifo /tmp/tmuxpipeworker
+tmux pipe-pane -o -t server 'cat >> /tmp/tmuxpipeserver'
+cat /tmp/tmuxpipeserver & 
 tmux new -s worker -d
 tmux send-keys -t worker 'source ~/venv/bin/activate && source origami.env && python manage.py runworker &' C-m &
+mkfifo /tmp/tmuxpipeworker
+tmux pipe-pane -o -t worker 'cat >> /tmp/tmuxpipeworker'
+cat /tmp/tmuxpipeworker & 
 tmux new -s node -d
-tmux send-keys -t node 'node server.js' C-m
+tmux send-keys -t node 'node server.js' C-m &
+mkfifo /tmp/tmuxpipenode
+tmux pipe-pane -o -t node 'cat >> /tmp/tmuxpipenode'
+cat /tmp/tmuxpipenode & 
+function finish {
+	tmux kill-session -t server
+	tmux kill-session -t worker
+	tmux kill-session -t node
+}
+trap finish EXIT
+sleep infinity
+
