ef71f81b973f9366d3f64a27e8ecf0064c30d953
PalashTanejaPro <tanejapalash@gmail.com>
2018-01-15 00:38:10 +0530

    prettier

--- Origami/src/components/deployment/UsePrebuiltProject/selectOutputComponentPage.js
+++ Origami/src/components/deployment/UsePrebuiltProject/selectOutputComponentPage.js
@@ -4,12 +4,12 @@ import { Link, browserHistory } from "react-router";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as nonghDemoModelActions from "../../../actions/nonghDemoModelActions";
-import * as outputComponentDemoModelActions
-  from "../../../actions/outputComponentDemoModelActions";
-import { getAllOutputComponentsForShowcase, getAllPreviewsForShowcase } from "../../outputcomponents";
+import * as outputComponentDemoModelActions from "../../../actions/outputComponentDemoModelActions";
 import {
-  getComponentDeployed
-} from "../../../api/CommonLocal/getComponentDeployed";
+  getAllOutputComponentsForShowcase,
+  getAllPreviewsForShowcase
+} from "../../outputcomponents";
+import { getComponentDeployed } from "../../../api/CommonLocal/getComponentDeployed";
 import { Step, Stepper, StepLabel } from "material-ui/Stepper";
 import { grey900 } from "material-ui/styles/colors";
 import toastr from "toastr";
@@ -34,7 +34,16 @@ class SelectOutputComponentPage extends React.Component {
   }
 
   componentWillMount() {
-    this.setState({headers: {"areagraph": [], "bargraph": [], "image": [], "piechart": [], "scattergraph": [], "text": [] }});
+    this.setState({
+      headers: {
+        areagraph: [],
+        bargraph: [],
+        image: [],
+        piechart: [],
+        scattergraph: [],
+        text: []
+      }
+    });
     getComponentDeployed(
       this.state.user_id,
       this.props.nonghDemoModel.id,
@@ -45,9 +54,8 @@ class SelectOutputComponentPage extends React.Component {
           let dataToSeed = {
             id: JSON.parse(outputComponentSeedData)[0].id,
             user_id: JSON.parse(outputComponentSeedData)[0].user_id,
-            base_component_id: JSON.parse(outputComponentSeedData)[
-              0
-            ].base_component_id,
+            base_component_id: JSON.parse(outputComponentSeedData)[0]
+              .base_component_id,
             props: JSON.parse(outputComponentSeedData)[0].props
           };
           this.setState({ outputComponentDemoModel: dataToSeed });
@@ -82,42 +90,42 @@ class SelectOutputComponentPage extends React.Component {
       dataToUpdate.push(value);
     });
     var tempdict = Object.assign({}, this.state.headers);
-    tempdict[type]=dataToUpdate;
+    tempdict[type] = dataToUpdate;
     this.setState({ headers: tempdict });
   }
   getHeaders(type) {
     let headers = [];
     this.state.headers[type].map((header, index) => {
       if (typeof header == "object") {
-         headers[index] = header["label"];
+        headers[index] = header["label"];
       } else {
-         headers[index] = header;
+        headers[index] = header;
       }
     });
     return headers;
   }
-  
-  togglePreview(){
-    this.setState({showPreview: !this.state.showPreview});
+
+  togglePreview() {
+    this.setState({ showPreview: !this.state.showPreview });
   }
 
   render() {
     const actions = [
-            <FlatButton
-              key={0}
-              label="Ok"
-              primary
-              keyboardFocused
-              onTouchTap={this.togglePreview}
-            />
-          ];
+      <FlatButton
+        key={0}
+        label="Ok"
+        primary
+        keyboardFocused
+        onTouchTap={this.togglePreview}
+      />
+    ];
     const updatefuncs = [
-      (data) => this.updateHeaders(data, "areagraph"),
-      (data) => this.updateHeaders(data, "bargraph"),
-      (data) => this.updateHeaders(data, "image"),
-      (data) => this.updateHeaders(data, "piechart"),
-      (data) => this.updateHeaders(data, "scattergraph"),
-      (data) => this.updateHeaders(data, "text")
+      data => this.updateHeaders(data, "areagraph"),
+      data => this.updateHeaders(data, "bargraph"),
+      data => this.updateHeaders(data, "image"),
+      data => this.updateHeaders(data, "piechart"),
+      data => this.updateHeaders(data, "scattergraph"),
+      data => this.updateHeaders(data, "text")
     ];
     const getfuncs = [
       () => this.getHeaders("areagraph"),
@@ -127,11 +135,10 @@ class SelectOutputComponentPage extends React.Component {
       () => this.getHeaders("scattergraph"),
       () => this.getHeaders("text")
     ];
-    document.body.scrollTop = (document.documentElement.scrollTop = 0);
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
 
     return (
       <div className="ui relaxed stackable grid fluid">
-
         <div className="ui relaxed stackable grid fluid container">
           <div
             style={{
@@ -162,40 +169,65 @@ class SelectOutputComponentPage extends React.Component {
             </div>
 
             <div className="ui horizontal divider row">
-              <span><hr /></span>
+              <span>
+                <hr />
+              </span>
             </div>
 
-            <div
-              className="fifteen wide column stretched stackable centered row"
-            >
+            <div className="fifteen wide column stretched stackable centered row">
               <div
                 className="ui three padded column stackable grid"
                 style={{ marginLeft: "3%", minHeight: "90vh" }}
               >
-                {getAllOutputComponentsForShowcase({
-                  demoModel: this.props.nonghDemoModel,
-                  user: this.props.user,
-                  outputComponentDemoModel: this.state.outputComponentDemoModel,
-                  outputComponentDemoModelActions: this.props.outputComponentDemoModelActions,
-                  forwardAddress: `/ngh/user/${this.props.user.id || localStorage.getItem("user_id")}/${this.props.nonghDemoModel.name}/${this.props.nonghDemoModel.id}/demo`,
-                  params: this.props.params,
-                  selected: this.state.outputComponentDemoModel.base_component_id
-                }, updatefuncs, this.state.headers).map((showcasecard, index) => showcasecard)}
-                
-                 {this.state.showPreview &&
-                   <Dialog
-                   title="Preview"
-                   actions={actions}
-                   modal
-                   autoScrollBodyContent
-                   open={this.state.showPreview}
-                   >
-                   {getAllPreviewsForShowcase(getfuncs).map((preview, index) => preview)}
-                   </Dialog>
-                 }
-                <div className="row" style={{marginTop: "-10vh", height: "6vh", minHeight: "6vh", width: "100%", marginLeft: "37%"}}>
-                  <RaisedButton label={"Preview"} primary={true} onClick={this.togglePreview}/>
-               </div>
+                {getAllOutputComponentsForShowcase(
+                  {
+                    demoModel: this.props.nonghDemoModel,
+                    user: this.props.user,
+                    outputComponentDemoModel: this.state
+                      .outputComponentDemoModel,
+                    outputComponentDemoModelActions: this.props
+                      .outputComponentDemoModelActions,
+                    forwardAddress: `/ngh/user/${this.props.user.id ||
+                      localStorage.getItem("user_id")}/${
+                      this.props.nonghDemoModel.name
+                    }/${this.props.nonghDemoModel.id}/demo`,
+                    params: this.props.params,
+                    selected: this.state.outputComponentDemoModel
+                      .base_component_id
+                  },
+                  updatefuncs,
+                  this.state.headers
+                ).map((showcasecard, index) => showcasecard)}
+
+                {this.state.showPreview && (
+                  <Dialog
+                    title="Preview"
+                    actions={actions}
+                    modal
+                    autoScrollBodyContent
+                    open={this.state.showPreview}
+                  >
+                    {getAllPreviewsForShowcase(getfuncs).map(
+                      (preview, index) => preview
+                    )}
+                  </Dialog>
+                )}
+                <div
+                  className="row"
+                  style={{
+                    marginTop: "-10vh",
+                    height: "6vh",
+                    minHeight: "6vh",
+                    width: "100%",
+                    marginLeft: "37%"
+                  }}
+                >
+                  <RaisedButton
+                    label={"Preview"}
+                    primary={true}
+                    onClick={this.togglePreview}
+                  />
+                </div>
               </div>
             </div>
           </div>
