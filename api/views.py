0f4cb0908a955872e724e697a21e40a2df1e937d
vpn1997 <mailme.vipin475@gmail.com>
2018-02-21 23:22:36 +0530

    added some view unittests

--- api/views.py
+++ api/views.py
@@ -194,11 +194,7 @@ def custom_component_controller(request, type_req, user_id, demoid):
     elif type_req == "output":
         model = OutputComponent
         serializer = OutputComponentSerializer
-    else:
-        return Response(
-            "Invalid URL",
-            status=response_status.HTTP_404_NOT_FOUND
-        )
+
 
     if request.method == "POST":
         body = request.data
@@ -214,6 +210,7 @@ def custom_component_controller(request, type_req, user_id, demoid):
                         "ascii", "ignore").decode('utf-8')})
             else:
                 props.append({})
+
         user_id = body["user_id"]
         component = model.objects.create(
             demo=demo, base_component_id=base_comp_id,
@@ -312,7 +309,7 @@ def custom_demo_controller(request, user_id, id):
             try:
                 demo = Demo.objects.get(id=id, user_id=user_id)
             except Exception:
-                return Response({"text": "Not Found"})
+                 return Response({"text": "Not Found"})
             serialize = DemoSerializer(demo).data
             try:
                 sample_inputs = SampleInput.objects.filter(demo=demo)
