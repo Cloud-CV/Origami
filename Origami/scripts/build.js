a75518bda9ca68b028e2d12cc7a58b231b346eca
Deepesh Pathak <deepshpathak@gmail.com>
2018-03-04 07:24:57 +0530

    feat(production-build): add and configure production build
    - refactor webpack config files
    - refactored package.json
    - add production build script

---
+++ Origami/scripts/build.js
@@ -0,0 +1,33 @@
+/*eslint-disable no-console*/
+
+import webpack from "webpack";
+import webpackConfig from "../../webpack.prod.config";
+import colors from "colors";
+
+process.env.NODE_ENV = "production";
+
+console.log(
+  "Generating minified bundle for production. This will take a moment...".blue
+);
+
+webpack(webpackConfig).run((err, stats) => {
+  if (err) {
+    console.log(err.bold.red);
+    return 1;
+  }
+
+  const jsonStats = stats.toJson();
+
+  if (jsonStats.hasErrors) {
+    return jsonStats.errors.map(error => console.log(error.red));
+  }
+
+  if (jsonStats.hasWarnings) {
+    console.log("Webpack generate the following warnings".bold.yellow);
+    jsonStats.warnings.map(warning => console.log(warning.yellow));
+  }
+
+  console.log("App compiled in production mode. Ready to roll!".green);
+
+  return 0;
+});
