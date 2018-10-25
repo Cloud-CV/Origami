64b1b86add0b08325977e8c011818797ddccc869
Haren <harensdeveloper@gmail.com>
2018-10-25 22:49:11 +0100

    Unicode Correction

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from allauth.socialaccount.models import SocialAccount, SocialToken, SocialApp
@@ -999,18 +999,18 @@ class DemoViewSetTests(TestCase):
 class ChannelTests(ChannelTestCase):
     def test_ws_connect(self):
         client = WSClient()
-        client.send_and_consume("websocket_connect")
+        client.send_and_consume("websocket.connect")
         self.assertIsNone(client.receive())
 
     def test_ws_message(self):
         client = WSClient()
-        client.send_and_consume('websocket_receive', text={'event': 'ConnectionEstablished', 'socketId': '54'})
+        client.send_and_consume('websocket.receive', text={'event': 'ConnectionEstablished', 'socketId': '54'})
         self.assertIsNone(client.receive())
 
-        client.send_and_consume('websocket_receive', text={'event': 'fetchCurrentPort'})
+        client.send_and_consume('websocket.receive', text={'event': 'fetchCurrentPort'})
         receive = client.receive()["data"]
         self.assertEqual(receive, settings.PORT)
 
-        client.send_and_consume('websocket_receive', text={'event': 'getPublicIPaddress'})
+        client.send_and_consume('websocket.receive', text={'event': 'getPublicIPaddress'})
         receive = client.receive()["data"]
         self.assertEqual(receive, settings.HOST_NAME)
