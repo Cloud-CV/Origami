016393d05ab78929a9b9af64aff32cdcc524901e
Parsa Ghadimi <me@qti3e.com>
2018-10-25 15:00:00 +0330

    Allow CORS request in webpack dev server

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -11,7 +11,12 @@ const config = {
   ],
   devServer: {
     inline: true,
-    hot: true
+    hot: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   },
   target: "web",
   output: {
