51504fa768ceeb939578a658f248a9fd41e60140
Sayam Kanwar <sayamkanwar616@gmail.com>
2018-10-27 08:11:32 +0530

    Added route to 404 not found error

--- Origami/src/components/nav_root.js
+++ Origami/src/components/nav_root.js
@@ -4,23 +4,42 @@ import HomePageComponent from "./home/HomePage";
 import NonGHUserProfileComponent from "./user/nonghUserProfile";
 import LoginComponent from "./stateless/login";
 import URLShortenerComponent from "./urlShortener";
+import PageNotfoundHandler from "./PageNotfoundHandler";
 import NGHDemoPageComponent from "./deployment/UsePrebuiltProject/nghDemoPage";
 import RegisterNonGHPageComponent from "./deployment/UsePrebuiltProject/registerPage";
 import ShareProfile from "./user/userShareProfile";
 import InitialSetupComponent from "./initialSetupPage";
 
 const routes = [
-  { path: '/home', name: 'Dashboard', component: HomePageComponent },
-  { path: '/', exact: true, name: 'Home', component: DefaultLayout },
-  { path: '/u/:username/:user_id', name: 'user', component: ShareProfile },
-  { path: '/p/:shorturl', exact: true, name: 'short', component: URLShortenerComponent },
-  { path: '/demo_register', exact: true, name: 'Create demo', component: RegisterNonGHPageComponent },
-  { path: '/login*', name: 'login', component: LoginComponent },
-  { path: '/initialsetup',exact: true, name: 'initialsetup', component:InitialSetupComponent },
-  { path: '/ngh/user',exact: true,name:'My Demo', component:NonGHUserProfileComponent },
-
+  { path: "/home", name: "Dashboard", component: HomePageComponent },
+  { path: "/", exact: true, name: "Home", component: DefaultLayout },
+  { path: "/u/:username/:user_id", name: "user", component: ShareProfile },
+  {
+    path: "/p/:shorturl",
+    exact: true,
+    name: "short",
+    component: URLShortenerComponent
+  },
+  {
+    path: "/demo_register",
+    exact: true,
+    name: "Create demo",
+    component: RegisterNonGHPageComponent
+  },
+  { path: "/login*", name: "login", component: LoginComponent },
+  {
+    path: "/initialsetup",
+    exact: true,
+    name: "initialsetup",
+    component: InitialSetupComponent
+  },
+  {
+    path: "/ngh/user",
+    exact: true,
+    name: "My Demo",
+    component: NonGHUserProfileComponent
+  },
+  { path: "*", name: "NotFound", component: PageNotfoundHandler }
 ];
 
-
 export default routes;
-
